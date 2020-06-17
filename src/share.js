a8a1dd3dafd6e28f52e2ff5349004dc0cec072ae
KFC <hankfchan@gmail.com>
2020-06-18 00:00:12 +0800

    ty message

--- src/share.js
+++ src/share.js
@@ -6,7 +6,7 @@ const mainShare = event => {
   const fbShare = () => {
     var baseURL = "https://www.facebook.com/sharer/sharer.php";
     var u =
-      "https://act.greenpeace.org/page/60860/petition/1?utm_campaign=2020-supermarket&utm_source=facebook&utm_medium=social&utm_content=main_share";
+      "https://act.greenpeace.org/page/62953/petition/1?utm_campaign=2020-climate&utm_source=facebook&utm_medium=social&utm_content=thankyou_page";
     var t = (window.innerHeight - 436) / 2;
     var l = (window.innerWidth - 626) / 2;
     window.open(
@@ -21,8 +21,8 @@ const mainShare = event => {
       .share({
         title: "",
         text:
-          "Hi👋你覺唔覺超級市場嘅商品被即棄塑膠包裝好無謂？綠色和平揭露，超市即棄包裝係塑膠污染嘅主要源頭之一；呢啲即棄塑膠包裝入侵食物鏈，海鮮、食鹽亦無一倖免！綠色和平推動到百佳已踏出減塑第一步，但加快超市包括惠康走塑的步伐，急需你的力量，立即聯署👉 ",
-        url: "https://act.gp/2Lu125t"
+          "Hi👋你覺唔覺香港愈嚟愈熱？又愈嚟愈多破壞力強嘅颱風？綠色和平最新評估顯示，假如全球溫室氣體排放趨勢不變，可能平均每10年就會受到類似山竹帶來嘅風暴潮侵襲😨應對氣候危機己經刻不容緩，急需你一同發聲，為拯救脆弱嘅氣候出一分力，立即聯署👉 ",
+        url: "https://act.gp/2zDwSKK"
       })
       .then()
       .catch();
@@ -34,6 +34,6 @@ const mainShare = event => {
 const whatsAppShare = event => {
   event.preventDefault();
   var w =
-    "https://api.whatsapp.com/send?text=Hi👋你覺唔覺超級市場嘅商品被即棄塑膠包裝好無謂？綠色和平揭露，超市即棄包裝係塑膠污染嘅主要源頭之一；呢啲即棄塑膠包裝入侵食物鏈，海鮮、食鹽亦無一倖免！綠色和平推動到百佳已踏出減塑第一步，但加快超市包括惠康走塑的步伐，急需你的力量，立即聯署👉 https://act.gp/2Z7JjJ8";
+    "https://api.whatsapp.com/send?text=Hi👋你覺唔覺香港愈嚟愈熱？又愈嚟愈多破壞力強嘅颱風？綠色和平最新評估顯示，假如全球溫室氣體排放趨勢不變，可能平均每10年就會受到類似山竹帶來嘅風暴潮侵襲😨應對氣候危機己經刻不容緩，急需你一同發聲，為拯救脆弱嘅氣候出一分力，立即聯署👉 https://act.gp/2N7uVta";
   window.open(w);
 };
