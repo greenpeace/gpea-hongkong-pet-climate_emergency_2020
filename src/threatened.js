bfaef6eee3608bfb8b7e4187c729191c2359574e
KFC <hankfchan@gmail.com>
2020-06-17 23:44:30 +0800

    before launch

---
+++ src/threatened.js
@@ -0,0 +1,29 @@
+const threadened = [
+  {
+    district: "元朗",
+    populations: 38000,
+    examples: "米埔，香港濕地公園，豐樂圍，南生圍，竹園村，壆圍村"
+  },
+  {
+    district: "北區",
+    populations: 19000,
+    examples: "蠔殼圍，担水坑，沙頭角中英街一帶"
+  },
+  {
+    district: "中西區",
+    populations: 11000,
+    examples: "堅彌地城新海旁，中山公園，西環貨運碼頭，港澳碼頭"
+  },
+  {
+    district: "南區",
+    populations: 6000,
+    examples: "鴨脷洲沿岸（包括海怡半島），香港仔海濱，深灣遊艇俱樂部"
+  },
+  {
+    district: "荃灣",
+    populations: 5900,
+    examples: "麗城花園至海濱花園一帶的海旁，荃灣海濱花園，汀九灣"
+  }
+];
+
+export { threadened };
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@
 module.exports = {
   // options...
   publicPath:
-    process.env.NODE_ENV === "production"
-      ? "https://api.greenpeace.org.hk/2020/plastic_supermarket/"
-      : "/"
+    process.env.NODE_ENV === "production" ? "https://api.greenpeace.org.hk/2020/climate2020/" : "/"
 };
