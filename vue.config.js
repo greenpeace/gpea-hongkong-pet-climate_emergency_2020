f3317f96d364ca1926dcde3abb7f820633a170ed
KFC <chris.chan@greenpeace.org>
2021-07-15 15:54:42 +0800

    update path

--- vue.config.js
+++ vue.config.js
@@ -1,31 +1,32 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
 // @see https://github.com/chrisvfritz/prerender-spa-plugin
 
 module.exports = {
   // options...
   publicPath:
-		process.env.NODE_ENV === "production"
-			? "https://api.greenpeace.org.hk/2020/petition/zh-hk.2020.climate.climate_emergency_2020.general.signup.na.mc/"
-			: "/",
+    process.env.NODE_ENV === "production" ? "https://api.greenpeace.org.hk/2020/climate" : "/",
 
-	outputDir: 'build',
+  outputDir: "build",
 
-	assetsDir: 'static',
+  assetsDir: "static",
 
-	filenameHashing: true,
+  filenameHashing: true,
 
-	configureWebpack:  {
-		plugins: process.env.NODE_ENV === 'production' ? [
-			new PrerenderSPAPlugin({
-				// Required - The path to the webpack-outputted app to prerender.
-				staticDir: path.join(__dirname, 'build'),
-				// Required - Routes to render.
-				routes: [ '/'],
-			})
-		] : []
-	},
-	devServer: {
-		disableHostCheck: true
-	}
+  configureWebpack: {
+    plugins:
+      process.env.NODE_ENV === "production"
+        ? [
+            new PrerenderSPAPlugin({
+              // Required - The path to the webpack-outputted app to prerender.
+              staticDir: path.join(__dirname, "build"),
+              // Required - Routes to render.
+              routes: ["/"]
+            })
+          ]
+        : []
+  },
+  devServer: {
+    disableHostCheck: true
+  }
 };
